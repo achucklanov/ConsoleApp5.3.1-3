c553f1aaa68e024e5bd230c27c98b1543375b76d
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-23 21:43:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var somName = "Евгения";
+            Console.WriteLine(somName);
+
+
+            string name;
+            GetName(ref name);
+
+            Console.WriteLine(somName);
+
+            Console.ReadKey();
+        }
+        static void GetName(ref string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+        }
+    }
+}
